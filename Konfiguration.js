7c8e2fff3f103615da13bac7ec2a31991d96a6f3
oders <joschaa@gmail.com>
2018-01-20 16:14:19 +0100

    Das Skript wird in hintergrundladen geladen, was aber nur funktionieren würde, wenn danach nicht die Seite von Ihrer Quelle geladen wird

--- Konfiguration.js
+++ Konfiguration.js
@@ -1,7 +1,12 @@
-var seitenliste = [
+var Seitenliste = [
     ["page1.htm",5000,""],
+    ["http://www.uni-greifswald.de",2000,"unigw.js"],
     ["page2.htm",3000,""],
     ["page3.htm",5000],
-    ["http://www.uni-greifswald.de",2000],
-    ["ht."] //das schafft er noch nicht!
-]
+    //["ht."] //das schafft er noch nicht!
+];
+
+var CONFIGINTERVAL = 40000; // Alle x Sekunden wird die Seitenliste abgerufen
+
+/* Anzeigezeit einer Seite, sofern in der Konfigurationsdatei nichts anderes angegeben ist */
+var standarddelay = 3000;
