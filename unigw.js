b4c0e55eb263086bda673bf857de03918e724bd3
oders <joschaa@gmail.com>
2018-01-20 16:14:47 +0100

    Das Skript wird in hintergrundladen geladen, was aber nur funktionieren würde, wenn danach nicht die Seite von Ihrer Quelle geladen wird

---
+++ unigw.js
@@ -0,0 +1,13 @@
+function outputContent() {
+
+    var html_ = this.window.document; //document.body.innerHTML;
+    console.log("html", html_);
+    var rahmen = document.getElementsByClassName("main-content")[0];
+    console.log("");
+    //var neuerInhalt = document.getElementById("c1044877").innerHTML;
+   // rahmen.innerHTML = neuerInhalt;
+    //document.body.innerHTML = neuerInhalt;
+
+}
+
+outputContent();
