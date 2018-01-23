f7575dfe03a4d8d53b4d5098419e664dd067a076
oders <joschaa@gmail.com>
2018-01-23 03:00:48 +0100

    Beispielskript funktioniert

--- unigw.js
+++ unigw.js
@@ -3,9 +3,25 @@ var outputContent = function() {
     var html_ = this.scripts    ; //document.body.innerHTML;
     console.log("html", html_);
     var rahmen = document.getElementsByClassName("main-content")[0];
-    console.log(rahmen);
+
+    /* Elemente ausblenden */
+    var Ausgeblendete = [];
+   
+    function displayNone(el) {
+        // Blendet ein DOM-Objekt aus, 
+        // speichert den Original-Displaywert in *.style.orgDisplay und 
+        // fügt es der Liste der Ausgeblendeten Ojekte hinzu 
+        if (typeof(el) == "object") { // check
+            el.style.orgDisplay = el.style.display == "undefined" ? "" : el.style.display;
+            Ausgeblendete.push(el);
+            el.style.display = "none";
+        }
+    }
+    
+    displayNone(document.getElementsByTagName("Header")[0]);
+    //console.log(rahmen);
     //var neuerInhalt = document.getElementById("c1044877").innerHTML;
-   // rahmen.innerHTML = neuerInhalt;
+    //rahmen.innerHTML = neuerInhalt;
     //document.body.innerHTML = neuerInhalt;
 
 }
